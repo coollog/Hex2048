071dbc22a14216a87012866bbf8cabd5ebc37a7b
Qingyang Chen <qingyang.chen@gmail.com>
2017-01-20 04:18:31 +0000

    rebalanced

--- js/Controller.js
+++ js/Controller.js
@@ -294,7 +294,7 @@ Controller.AnimatingState = class extends Controller.State {
     
     // Update board to be the result.
     this._controller.board.updateWithResult(this._collapsedResult);
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 2; i++) {
       // If cannot add the needed number of new blocks, then player lost
       if (this._controller.board.numberOpen() < 1) {
         this._lost();
